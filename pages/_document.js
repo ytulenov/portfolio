59b32f42a9ddcc86e9900180b1a6227b3ef45b32
ytulenov <86948036+ytulenov@users.noreply.github.com>
2025-03-18 21:09:33 -0700

    Fdfd

--- pages/_document.js
+++ pages/_document.js
@@ -1,19 +1,20 @@
-import { ColorModeScript } from '@chakra-ui/react'
-import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
-import theme from '../lib/theme'
+import { ColorModeScript } from '@chakra-ui/react';
+import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
+import theme from '../lib/theme';
 
 export default class Document extends NextDocument {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+        </Head>
         <body>
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
- 
