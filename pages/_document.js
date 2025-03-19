5a2acca4111170826ed332047b55225669740593
ytulenov <86948036+ytulenov@users.noreply.github.com>
2025-03-18 21:28:53 -0700

    dfA

--- pages/_document.js
+++ pages/_document.js
@@ -6,9 +6,7 @@ export default class Document extends NextDocument {
   render() {
     return (
       <Html lang="en">
-        <Head>
-          <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no" />
-        </Head>
+        <Head/>
         <body>
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <Main />
