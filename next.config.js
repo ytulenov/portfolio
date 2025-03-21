f0ea496174d037de41f2074ccc55fe5a0783b8fb
ytulenov <86948036+ytulenov@users.noreply.github.com>
2025-03-21 04:28:36 -0700

    fdFD

--- next.config.js
+++ next.config.js
@@ -8,18 +8,18 @@ const withMDX = require('@next/mdx')({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true, // Enable React strict mode
-  swcMinify: true, // Enable SWC minification for faster builds
+  reactStrictMode: true, 
+  swcMinify: true, 
   webpack(config) {
-    // Add SVG support using @svgr/webpack
+    
     config.module.rules.push({
       test: /\.svg$/,
       use: ['@svgr/webpack'],
     });
     return config;
   },
-  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'], // Moved inside nextConfig
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'], 
 };
 
-// Combine MDX configuration with the Next.js configuration
+
 module.exports = withMDX(nextConfig);
